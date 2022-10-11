ce05e7ecd81cfbf90cc6b7f4b036af9ed7b30456
19prince <darren@19prince.com>
2022-10-11 02:02:06 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,33 @@
+# _*_ coding: utf-8 __
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage Training""",
+    
+    'description': """
+        Academy Module to mange Training
+        - Courses
+        - Sessions
+        - Attenddess
+    """,
+    
+    'author': 'Darren O',
+    
+    'website': 'https://www.methodandmode.co',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+        
+    ],
+    
+    'demo': [
+        
+        
+    ],
+}
